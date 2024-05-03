b03b87e447ea61fe920d368107a2e1914b8c5d23
SaikoReyes <72665381+SaikoReyes@users.noreply.github.com>
2024-05-02 23:05:19 -0600

    Adición de reproductor para reproducir partitura.

--- src/MusicXMLViewer.jsx
+++ src/MusicXMLViewer.jsx
@@ -1,34 +1,61 @@
 import React, { useEffect, useRef } from 'react';
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
+import OSMDAudioPlayer from 'osmd-audio-player';
+import './ResultPage'
 
 const MusicXMLViewer = ({ musicXML }) => {
     const osmdContainerRef = useRef(null);
     const osmd = useRef(null);
+    const audioPlayer = useRef(new OSMDAudioPlayer());
 
     useEffect(() => {
-        if (!osmd.current && osmdContainerRef.current) {
-            osmd.current = new OpenSheetMusicDisplay(osmdContainerRef.current, {
-                autoResize: true,
-                backend: "svg",
-                drawingParameters: "compact",
-                drawPartNames: false,
-                drawMeasureNumbers: false,
-            });
-        }
+        const loadAndRenderMusic = async () => {
+            if (!osmd.current && osmdContainerRef.current) {
+                osmd.current = new OpenSheetMusicDisplay(osmdContainerRef.current, {
+                    autoResize: true,
+                    backend: "svg",
+                    drawingParameters: "compact",
+                    drawPartNames: false,
+                    drawMeasureNumbers: false,
+                });
+            }
 
-        if (osmd.current && musicXML) {
-            osmd.current
-                .load(musicXML)
-                .then(() => {
+            if (osmd.current && musicXML) {
+                try {
+                    await osmd.current.load(musicXML);
                     osmd.current.render();
-                })
-                .catch(error => console.error("Error loading or rendering musicXML", error));
-        }
+                    await audioPlayer.current.loadScore(osmd.current); 
+                } catch (error) {
+                    console.error("Error loading or rendering musicXML", error);
+                }
+            }
+        };
+
+        loadAndRenderMusic();
     }, [musicXML]);
 
-    return <div style={{ overflowY: 'auto', maxHeight: '80vh', backgroundColor: 'white' }}>
-        <div ref={osmdContainerRef} />
-    </div>;
+    const handlePlay = () => {
+        audioPlayer.current.play();
+    };
+
+    const handleStop = () => {
+        audioPlayer.current.pause();
+    };
+
+    return (
+        <div>
+            <div style={{ overflowY: 'auto', maxHeight: '80vh', backgroundColor: 'white' }}>
+                <div ref={osmdContainerRef} />
+            </div>
+            <button onClick={handlePlay} className="button-dark">
+                Reproducir
+            </button>
+            <button onClick={handleStop} className="button-dark">
+                Detener
+            </button>
+        </div>
+    );
+    
 };
 
 export default MusicXMLViewer;
